3a6bb076565c17f1d159bf5c1fb1eb6743668089
Brad <bradcopenhaver@gmail.com>
2017-09-11 18:32:53 -0700

    allow decimals in transaction fields

--- BankLedger/Program.cs
+++ BankLedger/Program.cs
@@ -20,7 +20,7 @@ namespace BankLedger
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls(urls: "http://*:80;http://*:8000")
+                //.UseUrls(urls: "http://*:80;http://*:8000")
                 .Build();
     }
 }
